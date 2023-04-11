2ef53084a4111561bcf9ca9901cca33fda3d26b9
lassandra85 <lassandra85@gmail.com>
2023-04-11 23:57:32 +0200

    add2

--- contacts.js
+++ contacts.js
@@ -1,21 +1,30 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+/* import fs from 'fs'; */
+/* import path from 'path'; */
+
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join( __dirname, 'db', 'contacts.json');
 
 // TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
-}
 
-function getContactById(contactId) {
-  // ...твій код
-}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(contacts);
+};
 
-function removeContact(contactId) {
-  // ...твій код
-}
+const getContactById = async (contactId) => {
+    const data = await listContacts();
+    const result = data.find(contact => contact.id === contactId);
+    return result;
+};
 
-function addContact(name, email, phone) {
+/* function removeContact(contactId) {
   // ...твій код
 }
+ */
+/* function addContact(name, email, phone) {
+  // ...твій код
+} */
+
+module.exports = {listContacts,getContactById,};
--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const fs = require('fs').promises;
-const path = require('path');
+/* import listContacts  from './contacts.js'; */
 
-(async() => )
+/* const { Command } = require("commander"); */
+const db = require("./db");
+
+/* const program = new Command(); */
+
+/* program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv); */
+
+/* const argv = program.opts(); */
+
+// TODO: рефакторить
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case "list":
+            const allContacts = await db.listContacts();
+            return console.log(allContacts);
+           /*  break; */
+
+        case "get":
+          // ... id
+          break;
+    
+        /* case "add":
+          // ... name email phone
+          break; */
+    
+        /* case "remove":
+          // ... id
+          break; */
+    
+        /* default:
+          console.warn("\x1B[31m Unknown action type!"); */
+    }
+};
+
+/* invokeAction(argv); */
+invokeAction({ action: "list"});
 
 
    
